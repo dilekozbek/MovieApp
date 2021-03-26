675bfde7e6603f94102f82b35eb900447668d676
Dilek Özbek <dilek_zbk@hotmail.com>
2021-03-26 23:33:10 +0300

    routing to views and dashboard page created

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,7 +1,11 @@
 import {Movie} from "./movie";
 
 export const Movies : Movie[] = [
-  {id: 1, name: "movie1"},
-  {id: 2, name: "movie2"},
-  {id: 3, name: "movie3"}
+  {id: 1, name: "movie1",description:"good movie", imageUrl:"1.jpg"},
+  {id: 2, name: "movie2",description:"bad movie", imageUrl:"2.jpg"},
+  {id: 3, name: "movie3",description:"perfect movie", imageUrl:"3.jpg"},
+  {id: 4, name: "movie4",description:"bad movie", imageUrl:"4.jpg"},
+  {id: 5, name: "movie5",description:"so-so movie", imageUrl:"5.jpg"},
+  {id: 6, name: "movie5",description:"nice movie", imageUrl:"6.jpg"},
+  {id: 7, name: "movie5",description:"good movie", imageUrl:"7.jpg"}
 ];
