a3ecdbfd14180c77fc48e04321bdf6b6ce084380
Dilek Özbek <dilek_zbk@hotmail.com>
2021-03-27 21:38:15 +0300

    showing movie details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MoviesDetailComponent } from './movies-detail/movies-detail.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo:'/dashboard', pathMatch:'full'},
   {path:'dashboard', component: DashboardComponent},
-  {path: 'movies', component: MoviesComponent}
+  {path: 'movies', component: MoviesComponent},
+  {path: 'detail/:id', component: MoviesDetailComponent}
 ];
 
 @NgModule({
